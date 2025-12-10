3ffd6c678b55c4d9f4dd54d719e59ad248f8c02f
Esteban Bedoya <mikeydoya@tuta.io>
2025-12-10 11:04:09 -0500

    .gitignore creado

--- frontend-service/src/index.jsx
+++ frontend-service/src/index.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 
 // API configuration
-window.API_BASE_URL = 'http://microservicios-alb-123456789.us-east-1.elb.amazonaws.com';
+window.API_BASE_URL = 'http://containers-alb-2124506834.us-east-1.elb.amazonaws.com';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
